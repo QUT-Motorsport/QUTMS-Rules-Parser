9ffb9a30c1f63a40fe767d77c0a9f0c707a4052a
John Hatfield <johnh@hattech.com.aui>
2022-04-11 20:46:41 +1000

    Wrapper for different rule codes

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ with open('text.txt', 'w') as f:
 with open('export.csv','w', newline='') as f:
     writer = csv.writer(f)
 
-
-    # Find all occurences of pattern that signifies a rule start
-    regex = re.findall('(EV(.|\n)+?)(?=\n*EV\.)',text)
-    
-    # Skip first one, as first occurence always is whole document
-    for x in range(2,len(regex)):
-
-        result = regex[x]
-
-        # regex rule code out of line result
-        ruleCode = re.search('(EV.+?) ', result[0])
+    for code in ["GR","AD","DR","F","V","T","VE","EV","IN"]:
+        # Find all occurences of pattern that signifies a rule start
+        regex = re.findall('(?!\n)('+code+'(.|\n)+?)(?=\n*'+code+'\.)',text)
         
-        if ruleCode:
-            # get text of rule code if present in result
-            ruleCodeText = ruleCode.groups()[0]
+        # Skip first one, as first occurence always is whole document
+        for x in range(2,len(regex)-1):
 
-            # trim off rule code from text
-            length = len(ruleCode.groups()[0])
-            ruleContent = result[0][length:]
+            result = regex[x]
 
-            # concat row for csv and write to csv
-            row = ruleCodeText, ruleContent
-            writer.writerow(row)
+            # regex rule code out of line result
+            ruleCode = re.search('('+code+'.+?) ', result[0])
+            
+            if ruleCode:
+                # get text of rule code if present in result
+                ruleCodeText = ruleCode.groups()[0]
+
+                # trim off rule code from text
+                length = len(ruleCode.groups()[0])
+                ruleContent = result[0][length:]
+
+                # concat row for csv and write to csv
+                row = ruleCodeText, ruleContent
+                writer.writerow(row)
